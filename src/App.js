f7f472367a105b8448c203c751d2cac1f6019bb7
Rizwan Zaheer <rizwanzaheer003@gmail.com>
2018-09-10 09:58:05 +0500

    adding second service

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          hi there
+          I was changed there.
         </p>
       </div>
     );
